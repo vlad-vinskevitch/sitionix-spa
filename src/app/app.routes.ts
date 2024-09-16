6e641b2662ef695daaaf9e688fe84d9a64e5dc7d
SerhiyKIT <sergii.kotik@gmail.com>
2024-09-16 17:17:40 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthSwitcherComponent } from './autorithation/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AuthSwitcherComponent }
+];
